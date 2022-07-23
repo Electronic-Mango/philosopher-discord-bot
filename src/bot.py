db6f0db66c52aeb31a7063890e3fae5734d4780f
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-07-23 21:06:23 +0200

    Initial commit
    Initial commit with the most basic bot functionality.

---
+++ src/bot.py
@@ -0,0 +1,35 @@
+from logging import INFO, basicConfig, getLogger
+from os import getenv
+
+from discord.ext.commands import Bot
+from discord.ext.commands.context import Context
+from dotenv import load_dotenv
+
+logger = getLogger("bot_main")
+basicConfig(format="[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s", level=INFO)
+
+load_dotenv()
+DISCORD_BOT_TOKEN = getenv("DISCORD_BOT_TOKEN")
+COMMAND_PREFIX = getenv("COMMAND_PREFIX")
+
+bot = Bot(COMMAND_PREFIX)
+
+
+@bot.event
+async def on_connect() -> None:
+    logger.info(f"[{bot.user}] connected")
+
+
+@bot.event
+async def on_ready() -> None:
+    logger.info(f"[{bot.user}] ready")
+
+
+@bot.command(name="philosophize")
+async def philosophize(context: Context) -> None:
+    channel = context.channel
+    logger.info(f"[{channel}] handling 'philosophize' command")
+    await channel.send("Hello there!")
+
+
+bot.run(DISCORD_BOT_TOKEN)
