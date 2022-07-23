c11a156378144dc0f2e7e2a53c139364b38653b5
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-07-23 22:26:16 +0200

    Use existing webhook instead of creating new one
    If an webhook of given name already exist use it, rather than creating new one.

--- src/webhook.py
+++ src/webhook.py
@@ -14,10 +14,22 @@ WEBHOOK_REMOVED_RESPONSE = getenv("WEBHOOK_REMOVED_RESPONSE")
 
 async def create_webhook(context: Context, channels_to_modify: dict[TextChannel, Webhook]) -> None:
     channel = context.channel
-    logger.info(f"[{channel.id}] adding webhook")
-    webhook = await channel.create_webhook(name=WEBHOOK_NAME)
+    webhook = await _get_existing_webhook(channel)
+    if not webhook:
+        webhook = await channel.create_webhook(name=WEBHOOK_NAME)
+        logger.info(f"[{channel.id}] added new webhook [{webhook}]")
+    else:
+        logger.info(f" [{channel.id}] using existing webhook [{webhook}]")
     channels_to_modify[channel] = webhook
     await context.send(WEBHOOK_CREATED_RESPONSE)
+
+
+async def _get_existing_webhook(channel: TextChannel) -> Webhook:
+    webhooks = await channel.webhooks()
+    for webhook in webhooks:
+        if webhook.name == WEBHOOK_NAME:
+            return webhook
+    return None
 
 
 async def remove_webhook(context: Context, channels_to_modify: dict[TextChannel, Webhook]) -> None:
