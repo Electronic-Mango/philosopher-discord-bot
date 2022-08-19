9a45bfde66483336f881292f17621f9ad0713fbe
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-08-19 20:44:17 +0200

    Fix checking for permissions in "all" command
    When needed permissions are not available command will still be available, but will return an ephemeral error to the user.

--- src/bot/slash_command/all.py
+++ src/bot/slash_command/all.py
@@ -5,17 +5,25 @@ No modification itself happens here.
 Upon second call to this command, the webhook is removed.
 
 This command is only available in servers, since webhook is required to send modified message.
+
+If bot doesn't have "manage_webhooks" and "manage_messages" permissions
+it will send back and ephemeral error with this information.
 """
 
+from logging import getLogger
 from disnake import CommandInteraction
-from disnake.ext.commands import Cog, slash_command, has_guild_permissions
+from disnake.ext.commands import Cog, slash_command, bot_has_guild_permissions
 
 from bot.webhook import create_new_webhook, remove_webhook, get_webhook
 
 
 class All(Cog):
+    def __init__(self) -> None:
+        super().__init__()
+        self._logger = getLogger(__name__)
+
     @slash_command(name="all", dm_permission=False)
-    @has_guild_permissions(manage_messages=True, manage_webhooks=True)
+    @bot_has_guild_permissions(manage_webhooks=True, manage_messages=True)
     async def all(self, interaction: CommandInteraction) -> None:
         """Toggle modification of all messages in current channel"""
         await interaction.response.defer()
@@ -24,3 +32,8 @@ class All(Cog):
             await remove_webhook(interaction, webhook)
         else:
             await create_new_webhook(interaction)
+
+    @all.error
+    async def all_error(self, interaction: CommandInteraction, error) -> None:
+        self._logger.warn(error)
+        await interaction.send(content=error, ephemeral=True)
